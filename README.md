445931cdbc899c8660fb89ed622fa60fc52d7595
Gytis Karčiauskas <gytis@karciauskas.lt>
2015-11-20 22:13:02 +0200

    First plugin version.

---
+++ README.md
@@ -0,0 +1,13 @@
+# content2summary
+
+Simply converts article's content to a short summary.
+
+## Requirements
+
+* BeautifulSoup
+
+`pip install BeautifulSoup4`
+
+## Configuration
+
+Plugin uses default setting `SUMMARY_MAX_LENGTH`.
---
+++ content2summary.py
@@ -0,0 +1,15 @@
+from pelican import signals
+from bs4 import BeautifulSoup
+
+def content2summary(generator):
+    max_length = generator.settings.get('SUMMARY_MAX_LENGTH')
+
+    for article in generator.articles:
+        if 'summary' not in article.metadata.keys():
+            summary = ' '.join(article.content.split()[:max_length]) + ' ...'
+            soup = BeautifulSoup(summary, "html.parser")
+            article.metadata['summary'] = soup.get_text()
+
+def register():
+    """ Register plugin """
+    signals.article_generator_finalized.connect(content2summary)
